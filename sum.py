fdbff1faba2e2f4a16488c7134a6b7e1871ec168
rsbads <152560209+rsbads@users.noreply.github.com>
2023-11-30 23:56:06 +0000

    [feat]: created sum_two_variable()

---
+++ sum.py
@@ -0,0 +1,19 @@
+def sum_two_number(
+    a:float,
+    b:float = 5
+) -> float: 
+    """ Sum two values.
+
+    Take two variable and sum.
+
+    Args:
+        a (float): First number.
+        b (float, optional): Second number. Defaults to 5.
+
+    Returns:
+        float: The sum of 'a' and 'b'.
+    """
+
+    number = a + b
+    
+    return number
