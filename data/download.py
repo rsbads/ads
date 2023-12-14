d584a851d3036ea1b28de3879b86a41b6ad2e32f
rsbads <152560209+rsbads@users.noreply.github.com>
2023-12-14 23:33:18 +0000

    [feat]: created download_data()

---
+++ data/download.py
@@ -0,0 +1,16 @@
+import yfinance as yf
+import pandas as pd
+
+def download_data (ticker:str) -> pd.DataFrame:
+    """_summary_
+
+    Args:
+        ticker (str): the ticker of financil asset.
+
+    Returns:
+        pd.DataFrame: a dataframe retrive from Yahoo Finance.
+    """
+    
+    data = yf.download(ticker)
+
+    return data
